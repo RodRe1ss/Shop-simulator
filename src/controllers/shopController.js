addf75e192866b4b81dfa1623059cb4c90215bbe
RodRe1ss <158602206+RodRe1ss@users.noreply.github.com>
2026-08-05 08:56:05 +0100

    Add Shop Controller

---
+++ src/controllers/shopController.js
@@ -0,0 +1,38 @@
+const shopServices = require("../services/shopServices");
+
+const create = async (req, res) => {
+  try {
+    const { playerId, name } = req.body;
+
+    const shop = await shopServices.createShop(playerId, name);
+
+    res.status(201).json(shop);
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+};
+
+const getById = async (id) => {
+  try {
+    const shop = await shopServices.getShopById(req.params.id);
+
+    if (!shop) {
+      res.status(404).json({
+        error: "Player not found",
+      });
+    }
+
+    res.status(200).json(shop);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  create,
+  getById,
+};
